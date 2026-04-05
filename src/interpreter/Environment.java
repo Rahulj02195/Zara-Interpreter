ad450161dbc66355a0aa5e18432f5d1ca022601f
Rahulj02195 <rahul2539raj123@gmail.com>
2026-04-05 23:45:57 +0530

    added Environment.java file

---
+++ src/interpreter/Environment.java
@@ -0,0 +1,19 @@
+package interpreter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Environment {
+    private Map<String,Object> variables=new HashMap<>();
+
+    public void set(String name,Object value) {
+        variables.put(name, value);
+    }
+    public Object get(String name) {
+        if (!variables.containsKey(name)) {
+            throw new RuntimeException("Variable not defined: "+ name);
+        }
+        return variables.get(name);
+    }
+
+}
