0e7d389d7922ee0c96005521e1c41e17e5d9ebd5
Ayush Yadav <ayushk22824@gmail.com>
2026-04-13 18:16:47 +0530

    feat: Added StringNode.java file

---
+++ src/ast/StringNode.java
@@ -0,0 +1,14 @@
+package ast;
+
+import interpreter.Environment;
+
+public class StringNode implements Expression {
+    private final String value;
+    public StringNode(String value) {
+        this.value = value;
+    }
+    public Object evaluate(Environment env) {
+        return value;
+    }
+
+}
